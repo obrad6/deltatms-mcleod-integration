c4bad71fd49fa256c00996eb65ba7b886ca8657b
Milos Obradovic <M.Obradovic@DeltaFreightSystems.com>
2023-12-07 17:20:56 -0600

    Comment out main in app.py.

--- app.py
+++ app.py
@@ -216,8 +216,8 @@ def post_customer():
             return response
 
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=8080)
 
 
